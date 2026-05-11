1bbc32b2a61659ac1c37cc1f3488969a521f97c5
spunkytechy <etiweluchidiebere@gmail.com>
2026-05-11 15:46:52 +0100

    Added CI/CD pipeline

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>CI/CD Pipeline Assignment</h1>
+      <h2>React App deployed on AWS</h2>
+      <p>Implemented using GitHub Actions</p>
     </div>
   );
 }
